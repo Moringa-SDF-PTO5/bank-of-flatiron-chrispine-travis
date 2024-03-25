6c64ec4b9590c8715758f6c31ea487663570f1f8
OchiengJr <chrispine.ochieng@student.moringaschool.com>
2024-03-25 14:57:16 +0300

    Phase 2 Code Challenge: Bank of Flatiron

--- public/src/components/App.js
+++ public/src/components/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import AccountContainer from "./components/AccountContainer";
+import AddTransactionForm from "./components/AddTransactionForm";
+import TransactionsList from "./components/TransactionList";
+import Transaction from "./components/Transaction";
+import Search from "./components/Search";
+import "./components/styles.css"; // Import the CSS file with correct syntax
 
 function App() {
   return (
     <div className="App">
-    <div>
-    <h1>Bank Of Flatiron</h1>
-    </div>
-    <AccountContainer/>
+      <div>
+        <h1>Bank Of Flatiron</h1>
+      </div>
+      <AccountContainer />
+      {/* You can include other components here if needed */}
     </div>
   );
 }
